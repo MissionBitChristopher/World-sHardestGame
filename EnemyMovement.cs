abefcf28cad3719bbec09e4f8aff0e40690991c9
MissionBitChristopher <55220393+MissionBitChristopher@users.noreply.github.com>
2019-12-08 14:38:57 -0800

    Add files via upload

---
+++ EnemyMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.gameObject.name == "enemy1" || this.gameObject.name == "enemy3")
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        else {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+       
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    { 
+        if(collision.gameObject.name == "player") {
+            player.transform.position = new Vector3(-5.68f, 2.2f, 0);
+        }
+        else {
+            speed *= -1;
+        }
+    }
+}
---
+++ NewBehaviourScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Rigidbody2D>().freezeRotation = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Input.GetAxisRaw ("Vertical") > 0 || Input.GetAxisRaw ("Vertical") < 0) &&
+			(Input.GetAxisRaw ("Horizontal") > 0 || Input.GetAxisRaw ("Horizontal") < 0)) {
+			transform.Translate(new Vector2 (Input.GetAxisRaw ("Horizontal") * Time.deltaTime * speed / Mathf.Sqrt(2), 
+				Input.GetAxisRaw ("Vertical") * Time.deltaTime *speed / Mathf.Sqrt(2)));
+		} // If the player presses vertical and horizontal movement keys (W,A,S,D or arrow keys), they move diaglonally.
+
+		if (Input.GetAxisRaw ("Vertical") == 0) {
+
+			if (Input.GetAxisRaw ("Horizontal") > 0.5 || Input.GetAxisRaw ("Horizontal") < 0.5) {
+
+				transform.Translate(new Vector2 (Input.GetAxisRaw ("Horizontal") * Time.deltaTime *speed, 0f));
+			}
+		} // Player can move horizontally.
+
+
+		if (Input.GetAxisRaw ("Horizontal") == 0) {
+
+			if (Input.GetAxis ("Vertical") > 0.5 || Input.GetAxis ("Vertical") < 0.5) {
+
+				transform.Translate (new Vector2 (0f, Input.GetAxisRaw ("Vertical") * Time.deltaTime* speed));
+			}
+		} // Player can move veritcally.
+	}
+}
